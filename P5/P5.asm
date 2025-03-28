e38748e2c46b549448e7c6776fc51db2657ef9e6
Angel <whoisslowy@gmail.com>
2025-03-28 15:17:53 -0700

    Laboratorio

---
+++ P5/P5.asm
@@ -0,0 +1,52 @@
+%include "../LIB/pc_iox.inc"
+
+section .bss
+    N resw 1  
+
+
+    section .text
+        global _start
+        extern pHex_dw
+_start:
+    ;a
+    mov ebx, 0x5C4B2A60
+    add ebx, 0x00219B50
+    push ebx
+    push eax
+    mov eax, ebx
+    call pHex_dw
+    pop eax
+
+    ;b
+    push bx
+
+    ;c
+    mov al, bl
+    mov cl, 8
+    mul cl
+    mov [N], ax
+
+    mov eax, [N] 
+    push eax     
+    call pHex_dw
+    pop eax
+
+    ;d
+    inc word [N]
+    push eax
+    mov eax, [N]
+    call pHex_dw
+    pop eax
+
+    ;f
+    add [N], ah
+    dec word [N]
+    push eax
+    mov eax, [N]
+    call pHex_dw
+    pop eax 
+    
+    mov eax, 1
+    int 0x80
+
+    
